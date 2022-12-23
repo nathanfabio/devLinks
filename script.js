39c9736168e76f132c3b64ed605bbef80c747049
Nathan Fabio <nathan_3_m@hotmail.com>
2022-12-23 15:40:29 -0300

    js added: switch button

---
+++ script.js
@@ -0,0 +1,13 @@
+const toggleBtn = document.querySelector('.switch button')
+const body = document.querySelector('body');
+const img = document.querySelector('.profile img');
+
+toggleBtn.addEventListener('click', () => {
+    body.classList.toggle('light');
+
+    if (body.classList.contains('light')) {
+        img.setAttribute('src', "./assets/avatar-light.png")
+    } else {
+        img.setAttribute('src', "./assets/avatar.png")
+    }
+})
